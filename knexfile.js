e3fe7e734636495b5abc3559f4e558389ec2fc7f
Kyle Gray <th33nd3r@gmail.com>
2017-01-30 11:39:38 -0700

    deployed to heroku

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,8 @@ module.exports = {
     connection: 'postgres://localhost/classifieds'
    },
   production: {
-    connection:process.env.DATABASE_URL
+    client:'pg',
+    connection:'postgres://ejkhhmvjfbanbr:c4b7fc6bde563f229e6e19ec4d69f6c3b55780eae650b9ef501e72698e58b5ec@ec2-54-225-242-74.compute-1.amazonaws.com:5432/d3r190c56c39aj'
   },
 
 };
